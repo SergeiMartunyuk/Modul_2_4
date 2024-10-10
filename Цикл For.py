1c01b04c1031f23fe93874b3708e77c41f334f01
SergeiMartunyuk <alessaralfinit@gmail.com>
2024-10-11 01:14:50 +0500

    Update Цикл For.py

--- Цикл For.py
+++ Цикл For.py
@@ -3,7 +3,7 @@ primes = []
 not_primes = []
 for i in range(len(numbers)):
     is_primes = True
-    if i == 1:
+    if i <= 1:
         continue
     for j in range(2, int(i ** 0.5) + 1):
         if i % j == 0:
