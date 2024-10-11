752994cadf06d184da443b98e81e1e12d9d61726
SergeiMartunyuk <alessaralfinit@gmail.com>
2024-10-11 18:48:10 +0500

    Update Цикл For.py

--- Цикл For.py
+++ Цикл For.py
@@ -1,17 +1,19 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
+index = 0
 for i in range(len(numbers)):
-    is_primes = True
-    if i <= 1:
+    index = numbers[i]
+    if index <= 1:
         continue
-    for j in range(2, int(i ** 0.5) + 1):
-        if i % j == 0:
-            is_primes = False
+    is_prime = True
+    for j in range(2, int(index ** 0.5) + 1):
+        if index % j == 0:
+            is_prime = False
             break
-    if is_primes:
-        primes.append(i)
+    if is_prime == True:
+        primes.append(index)
     else:
-        not_primes.append(i)
+        not_primes.append(index)
 print(primes)
 print(not_primes)
